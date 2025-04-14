372976a05961da2d3b3bc49beda884f3f3661dfe
mateus.costa <mateusespindola25@hotmail.com>
2025-04-14 20:32:50 -0300

    chore:first commit.

---
+++ src/main/java/com/beverage_app/beverage_distributor_api/BeverageDistributorApiApplication.java
@@ -0,0 +1,13 @@
+package com.beverage_app.beverage_distributor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageDistributorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageDistributorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beverage_app/beverage_distributor_api/BeverageDistributorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beverage_app.beverage_distributor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeverageDistributorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
