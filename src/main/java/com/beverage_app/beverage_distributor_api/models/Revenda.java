0e8e9504ce20ab4d30a35b751fa65335ec12f99e
mateus.costa <mateusespindola25@hotmail.com>
2025-04-14 20:47:59 -0300

    feat:added revenda entity and repository.

---
+++ src/main/java/com/beverage_app/beverage_distributor_api/models/Revenda.java
@@ -0,0 +1,46 @@
+package com.beverage_app.beverage_distributor_api.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+public class Revenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "CNPJ é obrigatório")
+    @Pattern(regexp = "\\d{14}", message = "CNPJ inválido")
+    private String cnpj;
+
+    @NotBlank(message = "Razão Social é obrigatória")
+    private String razaoSocial;
+
+    @NotBlank(message = "Nome Fantasia é obrigatório")
+    private String nomeFantasia;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @ElementCollection
+    private List<String> telefones;
+
+    @ElementCollection
+    private List<String> nomesContatos;
+
+    @ElementCollection
+    private List<String> enderecosEntrega;
+
+}
---
+++ src/main/java/com/beverage_app/beverage_distributor_api/repositories/RevendaRepository.java
@@ -0,0 +1,7 @@
+package com.beverage_app.beverage_distributor_api.repositories;
+
+import com.beverage_app.beverage_distributor_api.models.Revenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RevendaRepository  extends JpaRepository<Revenda, Long> {
+}
