0deb3d5cdcd34aeb895b75c410c5dcf84fe04ae6
mateus.costa <mateusespindola25@hotmail.com>
2025-04-15 18:56:16 -0300

    feat:added PedidoFornecedor endpoint.

---
+++ src/main/java/com/beverage_app/beverage_distributor_api/controllers/PedidoFornecedorController.java
@@ -0,0 +1,28 @@
+package com.beverage_app.beverage_distributor_api.controllers;
+
+import com.beverage_app.beverage_distributor_api.dtos.PedidoFornecedorRequestDTO;
+import com.beverage_app.beverage_distributor_api.dtos.PedidoFornecedorResponseDTO;
+import com.beverage_app.beverage_distributor_api.services.PedidoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/pedidoFornecedor")
+public class PedidoFornecedorController {
+
+    private final PedidoService pedidoService;
+
+    public PedidoFornecedorController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoFornecedorResponseDTO> createPedidoFornecedor(@RequestBody @Valid PedidoFornecedorRequestDTO pedidoFornecedorRequestDTO) {
+        PedidoFornecedorResponseDTO pedidoFornecedorResponseDTO = pedidoService.createPedidoFornecedor(pedidoFornecedorRequestDTO);
+        return ResponseEntity.ok(pedidoFornecedorResponseDTO);
+    }
+}
--- src/main/java/com/beverage_app/beverage_distributor_api/repositories/RevendaRepository.java
+++ src/main/java/com/beverage_app/beverage_distributor_api/repositories/RevendaRepository.java
@@ -3,7 +3,6 @@ package com.beverage_app.beverage_distributor_api.repositories;
 import com.beverage_app.beverage_distributor_api.models.Revenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface RevendaRepository  extends JpaRepository<Revenda, Long> {
