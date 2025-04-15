c3d4b039cc30118ba0cb03d80aa081592b43dd5b
mateus.costa <mateusespindola25@hotmail.com>
2025-04-15 00:03:09 -0300

    refactor:added NoArgsConstructor to Revenda entoty.

--- src/main/java/com/beverage_app/beverage_distributor_api/models/Revenda.java
+++ src/main/java/com/beverage_app/beverage_distributor_api/models/Revenda.java
@@ -7,12 +7,14 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Revenda {
 
